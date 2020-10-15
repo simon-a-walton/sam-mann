164adb9838d44df0e7b3d4b3179e3c0d5e2fd061
Simon Walton <saw29@hotmail.co.uk>
2020-10-15 15:18:59 +0100

    created an abstraction for repeated code in the contact list

---
+++ src/components/ContactContent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { colors } from "./../constants/StyleConstants";
+import { css } from 'glamor';
+
+
+class AppContent extends React.PureComponent {
+  static propsTypes = {
+    link: PropTypes.string,
+    children: PropTypes.node.isRequired,
+    containerClassName: PropTypes.string
+  };
+
+  render() {
+    return (
+      <li>
+        <a href={this.props.link}>
+          <span className="fa-li">
+            <i className={`${this.props.containerClassName} ${css({ color: colors.purple})}`} />
+          </span>
+          &nbsp;
+          {this.props.children}
+        </a>
+      </li>
+    );
+  }
+}
+
+export default AppContent;
+
--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -6,6 +6,7 @@ import Content from './../components/Content';
 import Map from './../components/Map';
 import { colors } from "./../constants/StyleConstants";
 import { css } from 'glamor';
+import ContactContent from "./../components/ContactContent";
 
 const Contact = () => (
 <>
@@ -16,38 +17,30 @@ const Contact = () => (
         <Content contentText="Want to find out more? Get in touch to arrange a free consultation today." />
       </div>
       <ul className="fa-ul list-unstyled contact-list" style={{fontSize:'calc(8px + 1.05vw)'}}>
-        <li>
-          <a href="mailto:samantha@realhealthmatters.co.uk?subject=Enquiry from Website">
-            <span className="fa-li">
-              <i className={`fas fa-envelope-open-text ${css({ color: colors.purple})}`} />
-            </span>
-            &nbsp;
-            samantha@realhealthmatters.co.uk
-          </a>
-        </li>
-        <li>
-          <a href="tel:07884404100">
-            <span className="fa-li">
-              <i className={`fas fa-phone ${css({ color: colors.purple})}`} />
-            </span>
-            &nbsp;
-            07884404100
-          </a>
-        </li>
-        <li>
-          <span className="fa-li">
-            <i className={`fas fa-map-pin ${css({ color: colors.purple})}`} />
-          </span>
-            &nbsp;
-            Liss, England, GU33 7HN
-        </li>
+        <ContactContent
+          link='mailto:samantha@realhealthmatters.co.uk?subject=Enquiry from Website'
+          containerClassName='fas fa-envelope-open-text'
+        >
+          samantha@realhealthmatters.co.uk
+        </ContactContent>
+         <ContactContent
+          link='tel:07884404100'
+          containerClassName='fas fas fa-phone'
+        >
+          07884404100
+        </ContactContent>
+         <ContactContent
+          link=''
+          containerClassName='fas fa-map-pin'
+        >
+          Liss, England, GU33 7HN
+        </ContactContent>
       </ul>
     </Col>
     <Col lg="4" align="center" className="pb-5">
       <Map />
     </Col>
   </Row>
-
 </>
 );
 
