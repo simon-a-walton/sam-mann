f55ba18283a33e87a5d4b456ddd33d355b7e6896
Simon Walton <saw29@hotmail.co.uk>
2020-11-12 12:07:24 +0000

    abstracted data into new file and mapped over it in the render for Health Coaching section

---
+++ src/data/coachingData.js
@@ -0,0 +1,25 @@
+export const coachingData = [
+  {
+    className: 'fas fa-carrot',
+    content: "We all know what we 'should' be doing; drinking more water and less alcohol, taking more exercise and eating more vegetables and less sugar. However, the information can be confusing and hard to put into practice"
+  },
+    {
+    className: 'fas fa-people-carry',
+    content: 'It helps to have a plan of action, unique to you and your circumstances. Health coaching acts as a guiding hand and provides you with a clear plan and dedicated support to help you follow that plan and get the results you want.'
+  },
+    {
+    className: 'fas fa-heart',
+    content: 'Your health is worth investing your time in. To not do so is a false economy, even if much of your life is spent looking after others or working, you will not be able to give anybody else your best if you are not feeling your best.'
+  }
+];
+
+export const expertiseData = [
+   {
+    className: 'fas fa-check-circle',
+    content: 'Menstrual Disorders'
+  },
+    {
+    className: 'fas fa-check-circle',
+    content: 'Weight Management'
+  },
+];
