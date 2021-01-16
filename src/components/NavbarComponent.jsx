2265279741426c127ae28a1ee5a27fd2f5ddc2c9
Simon Walton <saw29@hotmail.co.uk>
2021-01-16 15:19:01 +0000

    added new logo to Navbar

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { healthCoachingID, workWithMeID, aboutID, contactID } from './../constants/AppConstants';
 import { css } from 'glamor';
+import Logo from '../images/Logo-03.png';
 
 class NavbarComponent extends React.Component {
   constructor(props) {
@@ -30,9 +31,9 @@ class NavbarComponent extends React.Component {
     return (
       <header id='nav-bar' className={this.getClassName()}>
         <Navbar collapseOnSelect expand='lg'>
-          <Navbar {...css({ [`& :hover`]: { transform: 'scale(1.05)' } })}>
+          <Navbar {...css({ [`& :hover`]: { transform: 'scale(1.02)' } })}>
             <Navbar.Brand {...css({ paddingLeft: '8px' })} href='#top'>
-              Real Health Matters
+              <img src={Logo} alt='Real Health Matters Logo' height={30} width={'auto'} {...css({ 'padding-bottom': '5px' })} />
             </Navbar.Brand>
           </Navbar>
           <Navbar.Toggle aria-controls='responsive-navbar-nav' />
